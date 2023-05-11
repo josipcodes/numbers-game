d56e24cf3de1e2f7a3af068c3df443f8ce7e09cc
josipcodes <>
2023-05-11 10:55:55 +0000

    Connect script.js to html document

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,39 @@
-document.addEventListener("DOMContentLoaded", function () {
-    let buttons = document.getElementsByTagName("button");
-    for (let button of buttons) {
-        button.addEventListener("click", function () {
-            if (this.getAttribute("data-type") === "new-game") {
-                ADDFUNCTIONHERE();
-            } else if (this.getAttribute("data-type") === "rules") {
-                ADDFUNCTIONHERE();
-            } else if (this.getAttribute("data-type") === "about") {
-                ADDFUNCTIONHERE();
-            } else if (this.getAttribute("data-type") === "controls") {
-                ADDFUNCTIONHERE();
-            }
-        });
-    }
-});
+let newGameButton = document.getElementById("new-game");
+newGameButton.addEventListener("click", chooseDifficulty);
+
+function chooseDifficulty(event) {
+    let gameContent = document.getElementsByClassName("game-window").innerHtml;
+
+    gameContent = `<h1>Numbers Game</h1>
+    <h2>Difficulty Level</h2>
+    <button type="button" class="new-game-screen-button" data-type="beginner">Beginner</button>
+    <button type="button" class="new-game-screen-button" data-type="intermediate">Intermediate</button>
+    <button type="button" class="new-game-screen-button" data-type="expert">Expert</button>
+    `;
+}
+
+
+
+
+
+//document.addEventListener("DOMContentLoaded", function () {
+//    let buttons = document.getElementsByTagName("button");
+//
+//    for (let button of buttons) {
+//        button.addEventListener("click", function () {
+//            if (this.getAttribute("data-type") === "new-game") {
+//                chooseDifficulty();
+//            } else if (this.getAttribute("data-type") === "rules") {
+//                ADDFUNCTIONHERE();
+//            } else if (this.getAttribute("data-type") === "about") {
+//                ADDFUNCTIONHERE();
+//            } else if (this.getAttribute("data-type") === "controls") {
+//                ADDFUNCTIONHERE();
+//            }
+//        });
+//    }
+//});
+
+
+
+
