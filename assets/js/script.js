266673500fc7004ae1dcd4ba3de0bf00c6c5188b
josipcodes <>
2023-05-23 17:11:13 +0000

    Add else if case in provideHint to remove hint class

--- assets/js/script.js
+++ assets/js/script.js
@@ -451,28 +451,21 @@ function provideHint() {
     let choices = [];
     // Loop runs as many times as there are spans on the board, minus one as there is no need to check the last span.
     let startOfCheck = 0;
-    forloop:
+    let found;
     for (let x = 0; x < spans.length; x++) {
-        if (spans[x].classList.contains("hint")) {
+        if (spans[x].classList.contains("hint") && found !== true) {
             startOfCheck = spans[x].getAttribute("data-place");
             console.log(startOfCheck, "startofcheck");
             // console.log(startOfCheck, "startofcheck");
-            // spans[x].classList.remove("hint");
+            spans[x].classList.remove("hint");
             // console.log("removing previous hint", x);
-            break;
+            found = true;
+        } else if (found === true) {
+            spans[x].classList.remove("hint");
         }
-        // if (spans[x].classList.contains("hint")) {
-        //     spans[x].classList.remove("hint");
-        //     console.log("removing hint", x);
-        // }
     }
-    // for (let x = 0; x < spans.length; x++) {
-    //     if (spans[x].classList.contains("hint")) {
-    //         spans[x].classList.remove("hint");
-    //     }
-    // }
     loopOne:
-    for (let i = startOfCheck; i < spans.length - 2; i++) {
+    for (let i = startOfCheck; i < spans.length - 1; i++) {
         // Loop runs as many times as there are spans on the board, counting from the current i + 1.
         for (let j = i + 1; j < spans.length; j++) {
             let spanIValue = Number(spans[i].innerHTML);
